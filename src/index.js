587fe6d6668b3966229f1acbe5603c067f60854b
Ge Guo <alex@Ges-MacBook-Pro.local>
2019-04-04 22:32:20 +1100

    updated

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import createSagaMiddleware from "redux-saga";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducers from "./reducers";
 import App from "./components/App";
 import rootSaga from "./saga";
 
 const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(rootReducers, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  rootReducers,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 // const store = createStore(rootReducers);
 
 sagaMiddleware.run(rootSaga);
